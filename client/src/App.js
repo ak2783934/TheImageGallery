7f30682c0b818959a9739a8ed2b568f5ba2bb0b9
ak2783934 <ak2783934@gmail.com>
2021-01-06 17:14:27 +0530

    With the CSS and BOOTSTRAP

--- client/src/App.js
+++ client/src/App.js
@@ -36,10 +36,13 @@ class App extends React.Component {
   submit = (event) => {
     event.preventDefault();
     var axData = new FormData();
+
     if (this.state.img === "") {
+      //Checking if the image is inserted
       alert("Please select an image!!");
       return;
     } else {
+      //Checking the size of image here
       if (this.state.img.size >= 2000000) {
         alert("Image size exceddd!");
         return;
@@ -81,7 +84,10 @@ class App extends React.Component {
       <div className="row mx-2">
         {posts.map((post, index) => (
           <div key={index}>
-            <div className="card mx-1 my-1" style={{ width: "18rem" }}>
+            <div
+              className="card mx-2 mt-2 bg-dark text-white border border-dark"
+              style={{ width: "18rem" }}
+            >
               <img
                 className="card-img-top mx-auto mt-1"
                 src={post.img.url}
@@ -89,8 +95,10 @@ class App extends React.Component {
                 style={{ width: "17rem", height: "200px" }}
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">{post.body}</p>
+                <h5 className="card-title">ABOUT</h5>
+                <p className="card-text text-secondary text-uppercase">
+                  {post.body}
+                </p>
               </div>
             </div>
           </div>
@@ -102,48 +110,45 @@ class App extends React.Component {
   render() {
     // console.log("State is :", this.state);
     return (
-      <div className="app mx-4 ">
-        <h2 className="text-center"> Upload and say!</h2>
+      <div className="app mx-4 center">
+        <h1 className="text-center"> Upload and say!</h1>
+
+        {/* Form creation is here */}
         <form
           method="POST"
-          className="col-lg p-0 input-group"
+          className="col-lg p-0 mx-2 input-group"
           encType="multipart/form-data"
         >
-          <div className="input-group-prepend">
-            <span className="input-group-text" aria-label="Image upload!">
-              Images Upload!
-            </span>
-          </div>
+          {/* This is image input, we can't equate it to a value because it was returning some error while loading the picture */}
+
           <div className="form-input ">
+            <label for="">Upload Image</label>
             <input
               type="file"
               name="img"
               onChange={this.handleChange("img")}
               placeholder="Post the image!"
               required
-              accept="image"
+              accept="image/*"
             />
           </div>
 
-          <div className="input-group-prepend">
-            <span className="input-group-text" aria-label="About!">
-              About!
-            </span>
-          </div>
+          {/* This is text input  */}
           <div className="form-input">
             <textarea
               name="body"
               value={this.state.body}
-              placeholder="Say about the image"
+              placeholder="Write!"
               onChange={this.handleChange("body")}
               required
             ></textarea>
           </div>
+
           <button
-            className="btn btn-success btn-block  m-1"
+            className="btn btn-secondary btn-block borderm-0"
             onClick={this.submit}
           >
-            Submit
+            SUBMIT
           </button>
         </form>
 
